80ca07be06123ed5e79d25fa5be2baaccc3eb8c7
Tissem-Ben <t.bendahma@gmail.com>
2024-08-06 09:58:43 +0200

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -23,7 +23,7 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_sheap_t;
+typedef struct binary_tree_s heap_t;
 
 /*prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
