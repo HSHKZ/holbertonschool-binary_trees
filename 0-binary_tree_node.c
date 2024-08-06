425e9373cf01e51178eba74b6c3eca044a6ca670
Tissem-Ben <t.bendahma@gmail.com>
2024-08-06 10:45:09 +0200

    Fix: update header and add files for binary tree operations

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ *  * binary_tree_node - Creates a binary tree node
+ *   * @parent: Pointer to the parent node of the node to create
+ *    * @value: Value to put in the new node
+ *     *
+ *      * Return: pointer to the new node, or NULL on failure
+ *       */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
