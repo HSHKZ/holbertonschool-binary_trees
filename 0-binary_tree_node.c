a6ce2a19009c5db3645d40c19343e7f1df5d8692
Tissem-Ben <t.bendahma@gmail.com>
2024-08-07 15:04:48 +0200

    Update 0-binary_tree_node.c and README.md with recent changes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,12 +2,12 @@
 #include "binary_trees.h"
 
 /**
- *  * binary_tree_node - Creates a binary tree node
- *   * @parent: Pointer to the parent node of the node to create
- *    * @value: Value to put in the new node
- *     *
- *      * Return: pointer to the new node, or NULL on failure
- *       */
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-holbertonschool-binary_trees project with Ibtissem
+# Holberton School Binary trees 
+This project contains implementations of various functions to manipulate binary trees in C. It is part of the Holberton School curriculum.
+
+## Description
+
+The project focuses on implementing basic operations on binary trees, including node creation and traversal. It also includes specific functionality to find the uncle of a node in a binary tree.
