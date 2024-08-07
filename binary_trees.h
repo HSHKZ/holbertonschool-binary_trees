8e1b1d2e22f3a4c85cef8bd39ceeac9b495f6f67
HSHKZ <lucasolegniel@gmail.com>
2024-08-07 16:24:21 +0200

    binary_trees.h updated for tasks

--- binary_trees.h
+++ binary_trees.h
@@ -48,5 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+size_t tree_height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /*BINARY_TREES_H */
